91e32787ef9f5fbc054fb85b2662469d7cc2c424
angelogic <jadecritx@gmail.com>
2022-10-19 17:12:06 -0400

    1019 change

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ class Config(object):
     BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'chaucontainer'
 
     SQL_SERVER = os.environ.get(
-        'SQL_SERVER') or 'chauserver1014.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'chaudb'
+        'SQL_SERVER') or 'serverchaunow.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'chausqldb '
     SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'chauadmin'
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Admin@chou88'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
@@ -21,15 +21,17 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "weU8Q~WNAgJFm5cTbdB-cwEpb-TlN_SVEVdApaFl"
+    CLIENT_SECRET = "-xL8Q~BsEnT40y45l_QjRD.M8HgoOEo3uV0WBaeh"
     # In your production app, Microsoft recommends you to use other ways to store your secret,
-    # such as KeyVault, or environment variable as described in Flask's documentation here:
+    # such as KeyVault,CLIENT_SECRET or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
-    # CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+    # CLIENT_SECRET = os.getenv(
+    #     "CLIENT_SECRET") or "-xL8Q~BsEnT40y45l_QjRD.M8HgoOEo3uV0WBaeh"
     # if not CLIENT_SECRET:
     #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-    AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app, else put tenant name
+    # For multi-tenant app, else put tenant name
+    AUTHORITY = "https://login.microsoftonline.com/af869596-a9aa-4e35-b5f9-603fdc2b33ec"
     # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
     CLIENT_ID = "fbf74638-48cb-4848-8842-9efda4a45365"
